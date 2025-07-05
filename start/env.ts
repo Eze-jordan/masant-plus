da5abc9e99d44e76496a14a17e4233ee4240478e
unknown <elieboulingui2@gmail.com>
2025-07-05 20:31:18 +0100

    udpate inertia

--- start/env.ts
+++ start/env.ts
@@ -4,9 +4,10 @@
 |--------------------------------------------------------------------------
 |
 | The `Env.create` method creates an instance of the Env service. The
-| service validates the environment variables and also cast values
+| service validates the environment variables and also casts values
 | to JavaScript data types.
 |
+
 */
 
 import { Env } from '@adonisjs/core/env'
@@ -16,25 +17,30 @@ export default await Env.create(new URL('../', import.meta.url), {
   PORT: Env.schema.number(),
   APP_KEY: Env.schema.string(),
   HOST: Env.schema.string({ format: 'host' }),
-  LOG_LEVEL: Env.schema.string(),
+  LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
 
   /*
-  |----------------------------------------------------------
-  | Variables for configuring session package
-  |----------------------------------------------------------
-  */
-  SESSION_DRIVER: Env.schema.enum(['cookie', 'memory'] as const),
-
-  /*
-  |----------------------------------------------------------
+  |--------------------------------------------------------------------------
   | Variables for configuring database connection
-  |----------------------------------------------------------
+  |--------------------------------------------------------------------------
   */
   DB_HOST: Env.schema.string({ format: 'host' }),
   DB_PORT: Env.schema.number(),
   DB_USER: Env.schema.string(),
   DB_PASSWORD: Env.schema.string.optional(),
   DB_DATABASE: Env.schema.string(),
+
+  /*
+  |--------------------------------------------------------------------------
+  | Variables for configuring the limiter package
+  |--------------------------------------------------------------------------
+  */
+  LIMITER_STORE: Env.schema.enum(['redis', 'memory'] as const),
+
+  // Ces variables sont optionnelles pour éviter l'erreur si Redis n'est pas utilisé
+  REDIS_HOST: Env.schema.string.optional({ format: 'host' }),
+  REDIS_PORT: Env.schema.number.optional(),
+  REDIS_PASSWORD: Env.schema.string.optional(),
 
   /*
   |----------------------------------------------------------
