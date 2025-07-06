75842c1acd34fe3c5af3a82e97f2560584535b2c
unknown <elieboulingui2@gmail.com>
2025-07-06 02:17:52 +0100

    udpate innertia

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { getDirname } from '@adonisjs/core/helpers'
 import inertia from '@adonisjs/inertia/client'
+import react from '@vitejs/plugin-react'
+import adonisjs from '@adonisjs/vite/client'
 
 export default defineConfig({
-  plugins: [
-    inertia({ ssr: { enabled: false } }), // Désactive SSR
-    react(),
-  ],
+  plugins: [inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.tsx' } }), react(), adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] })],
+
+  /**
+   * Define aliases for importing modules from
+   * your frontend code
+   */
   resolve: {
     alias: {
-      '~/': '/inertia/',
+      '~/': `${getDirname(import.meta.url)}/inertia/`,
     },
   },
 })
