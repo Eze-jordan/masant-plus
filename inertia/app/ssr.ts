59c7809c70c60c10016ef7765040217ce9bc3505
unknown <elieboulingui2@gmail.com>
2025-07-07 16:59:55 +0100

    update ssr

--- inertia/app/ssr.ts
+++ inertia/app/ssr.ts
@@ -1,20 +1,14 @@
-import { createInertiaApp} from '@inertiajs/vue3'
-import { createSSRApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
 import { renderToString } from '@vue/server-renderer'
-import { DefineComponent } from 'vue'
-import { Page } from '@inertiajs/core'
-import { PageProps } from './types'
+import { createSSRApp, h, type DefineComponent } from 'vue'
 
-// Si tu as une définition de type pour PageProps dans ton projet, assure-toi de l'importer ici
-
-// Mettre à jour la signature de la fonction render
-export default function render(page: Page<PageProps>) {  // Utiliser le type Page<PageProps>
+export default function render(page:any) {
   return createInertiaApp({
     page,
-    render: renderToString,  // Utilisation de renderToString pour SSR
-    resolve: (name: string) => {  // Assurez-vous que `name` est bien une chaîne
+    render: renderToString,
+    resolve: (name) => {
       const pages = import.meta.glob<DefineComponent>('../pages/**/*.vue')
-      return pages[`../pages/${name}.vue`]().then((module) => module.default)
+      return pages[`../pages/${name}.vue`]()
     },
     setup({ App, props, plugin }) {
       return createSSRApp({ render: () => h(App, props) }).use(plugin)
