eb1fe0407319ef3001eadf89ba7ca41041ccd35a
unknown <elieboulingui2@gmail.com>
2025-07-16 11:00:23 +0100

    update user sending email

--- app/services/MailFordoctor.ts
+++ app/services/MailFordoctor.ts
@@ -12,17 +12,17 @@ export default class MailService {
   })
 
   // Fonction dédiée pour envoyer le mail d'approbation
-  static async sendApprovalEmail(recipientEmail: string) {
+  static async sendApprovalEmail(first_name: string) {
     const html = `
-      <p>Bonjour ${recipientEmail}</p>
-      <p>Votre demande a été approuvée avec succès.</p>
+      <p>Bonjour ${first_name}</p>
+      <p>Votre demande a été enregister avec succès.</p>
       <p>Un administrateur vous contactera bientôt pour vous fournir toutes les commodités nécessaires.</p>
       <p>Merci de votre confiance.</p>
     `
 
     return await this.transporter.sendMail({
       from: '"Support AdonisJS" <elieboulingui2@gmail.com>',
-      to: recipientEmail,  // Use the dynamic email address passed to the function
+      to: first_name,  // Use the dynamic email address passed to the function
       subject: 'Votre demande a été approuvée',
       html,
     })
