c39337304981152708f22981b16dbdbdc29c6920
unknown <elieboulingui2@gmail.com>
2025-08-06 14:49:13 +0100

    update innertia

--- inertia/app/ssr.ts
+++ inertia/app/ssr.ts
@@ -1,17 +1,18 @@
+
 import { createInertiaApp } from '@inertiajs/vue3'
 import { renderToString } from '@vue/server-renderer'
 import { createSSRApp, h, type DefineComponent } from 'vue'
 
-export default function render(page:any) {
+export default function render(page: any) {
   return createInertiaApp({
     page,
     render: renderToString,
     resolve: (name) => {
-      const pages = import.meta.glob<DefineComponent>('../pages/**/*.vue')
-      return pages[`../pages/${name}.vue`]()
+      const pages = import.meta.glob<DefineComponent>('../pages/**/*.vue', { eager: true })
+      return pages[`../pages/${name}.vue`]
     },
-    setup({ App, props, plugin }) { 
-        
+
+    setup({ App, props, plugin }) {
       return createSSRApp({ render: () => h(App, props) }).use(plugin)
     },
   })
--- start/routes.ts
+++ start/routes.ts
@@ -3709,3 +3709,4 @@ router.on('/').renderInertia('home')
 
 
 
+
