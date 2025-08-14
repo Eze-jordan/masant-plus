b0a5b3a6853b077296ee6d96476afd446b969165
unknown <elieboulingui2@gmail.com>
2025-08-14 11:11:33 -0300

    udpate user lives

--- app/models/live.ts
+++ app/models/live.ts
@@ -1,5 +1,5 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany, beforeCreate } from '@adonisjs/lucid/orm'
 import { randomUUID } from 'node:crypto'
 import LiveUser from './liveUser.js' // ou ./LiveUser si c’est le vrai nom du fichier
 import type { HasMany } from '@adonisjs/lucid/types/relations'
@@ -11,6 +11,13 @@ export default class Live extends BaseModel {
   @column()
   public name!: string
 
+  // Nouveaux champs pour le doctor et patient
+  @column()
+  public doctorId!: string
+
+  @column()
+  public patientId!: string
+
   @column.dateTime({ autoCreate: true })
   public createdAt!: DateTime
 
@@ -19,9 +26,12 @@ export default class Live extends BaseModel {
 
   @hasMany(() => LiveUser)
   public liveUsers!: HasMany<typeof LiveUser>
-  static assignUuid: (live: Live) => void
-}
 
-Live.assignUuid = function (live: Live) {
-  live.id = randomUUID()
+  /**
+   * Hook before creating a live to assign a UUID
+   */
+  @beforeCreate()
+  public static assignUuid(live: Live) {
+    live.id = randomUUID()
+  }
 }
