229f7d549603ce24d2e9a71858d93ed36d56d0e9
unknown <Eze-jordan>
2025-11-13 23:18:38 +0100

    mail

--- app/services/MailService.ts
+++ app/services/MailService.ts
@@ -3,12 +3,13 @@ import nodemailer from 'nodemailer'
 
 export default class MailService {
   private static transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',       // ← Fixé ici
-    port: 587,
-    secure: false,                // TLS
+    service: 'gmail', // ← PLUS FIABLE QUE host + port
     auth: {
-      user: 'elieboulingui2@gmail.com',    // ← Ton adresse Gmail
-      pass: 'ozdf cset gqcr ofsd',       // ← Ton mot de passe d'application Gmail
+      user: 'elieboulingui2@gmail.com',
+      pass: 'ozdf cset gqcr ofsd', // ← Ton mot de passe d’application (16 caractères)
+    },
+    tls: {
+      rejectUnauthorized: false,
     },
   })
 
@@ -17,16 +18,15 @@ export default class MailService {
       <p>Bonjour,</p>
       <p>Voici votre code de réinitialisation :</p>
       <h2>${code}</h2>
-      <p>Ce code est valable pendant 10 minutes.</p>
-      <p>Si vous n’avez pas demandé ce changement, ignorez cet e-mail.</p>
+      <p>Il expire dans 10 minutes.</p>
+      <p>Si vous n’êtes pas à l’origine de cette demande, ignorez cet email.</p>
     `
 
     return await this.transporter.sendMail({
-      from: '"Support AdonisJS" <eliebouligui2@gmail.com>',
+      from: '"Support MASANTE+" <elieboulingui2@gmail.com>',
       to,
-      subject: 'Votre code de réinitialisation de mot de passe',
+      subject: 'Votre code OTP de réinitialisation',
       html,
     })
   }
 }
- 
